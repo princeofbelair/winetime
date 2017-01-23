4bf2c43d7b0726e445644ad9c05d1cde06343b8f
Romana Lechner <lechner.romana@gmail.com>
2017-01-23 19:52:06 +0100

    add few details

--- web/wineweb/src/controller/WineController.java
+++ web/wineweb/src/controller/WineController.java
@@ -36,10 +36,10 @@ public class WineController implements Serializable {
         return this.searchString;
     }
 
-    public List<Wine> getWines() {
+    /*public List<Wine> getWines() {
         return wine.searchForSubstring(this.searchString);
 
-    }
+    }*/
 
     private Wine selectedWine;
 
@@ -58,7 +58,7 @@ public class WineController implements Serializable {
      * @param resultList
      * @return
      */
-    private static List<String> getRegionsFromSearchResult(List<Wine> resultList) {
+    private List<String> getRegionsFromSearchResult(List<Wine> resultList) {
         List<String> regions = new ArrayList<>();
         for (Wine w : resultList) {
             regions.add(w.getWineRegion());
@@ -74,7 +74,7 @@ public class WineController implements Serializable {
      * @param resultList
      * @return
      */
-    private static List<String> getGrowerFromSearchResult(List<Wine> resultList) {
+    private List<String> getGrowerFromSearchResult(List<Wine> resultList) {
         List<String> growers = new ArrayList<>();
         for (Wine w : resultList) {
             growers.add(w.getWineGrower());
@@ -90,7 +90,7 @@ public class WineController implements Serializable {
      * @param resultList
      * @return
      */
-    private static List<String> getLocalityFromSearchResult(List<Wine> resultList) {
+    private List<String> getLocalityFromSearchResult(List<Wine> resultList) {
         List<String> localities = new ArrayList<>();
         for (Wine w : resultList) {
             localities.add(w.getLocality());
@@ -99,26 +99,48 @@ public class WineController implements Serializable {
         return locality;
     }
 
+    private List<String> getWineGrapeFromSearchResult(List<Wine> resultList) {
+        List<String> wineGrape = new ArrayList<>();
+        for (Wine w : resultList) {
+            wineGrape.add(w.getLocality());
+        }
+        List<String> wineGrapes = wineGrape.stream().distinct().collect(Collectors.toList());
+        return wineGrapes;
+    }
 
-    private static Map<String, List<String>> semanticSearch(String word) {
+    private List<String> getWineCategoryFromSearchResult(List<Wine> resultList) {
+        List<String> wineCategory = new ArrayList<>();
+        for (Wine w : resultList) {
+            wineCategory.add(w.getLocality());
+        }
+        List<String> wineCategories = wineCategory.stream().distinct().collect(Collectors.toList());
+        return wineCategories;
+    }
+
+
+    private Map<String, List<String>> semanticSearch(String word) {
         Map<String, List<String>> result = new HashMap<String, List<String>>();
 
         //sparql
         List<String> subClass = wine.querySubClass(word);
-        //if (!subClass.isEmpty()) result.put("subClass", subClass);
+        if (!subClass.isEmpty()) result.put("subClass", subClass);
         List<String> superClass = wine.querySuperClass(word);
-        //if (!subClass.isEmpty()) result.put("superClass", superClass);
+        if (!subClass.isEmpty()) result.put("superClass", superClass);
         List<String> synonyms = wine.queryEquivalentClass(word);
-        //if (!synonyms.isEmpty()) result.put("synonyms", synonyms);
+        if (!synonyms.isEmpty()) result.put("synonyms", synonyms);
 
         //db
-        List<Wine> dbResults = wine.searchForSubstring(word);
+        List<Wine> dbResults = wine.searchForSubstring(word, "", "", "");
         List<String> locality = getLocalityFromSearchResult(dbResults);
-        //if (!locality.isEmpty()) result.put("locality", locality);
+        result.put("locality", locality);
         List<String> growers = getGrowerFromSearchResult(dbResults);
-        //if (!growers.isEmpty()) result.put("growers", growers);
+        result.put("growers", growers);
         List<String> regions = getRegionsFromSearchResult(dbResults);
-        //if (!regions.isEmpty()) result.put("regions", regions);
+        result.put("regions", regions);
+        List<String> wineGrapes = getWineGrapeFromSearchResult(dbResults);
+        result.put("wineGrapes", wineGrapes);
+        List<String> wineCategories = getWineCategoryFromSearchResult(dbResults);
+        result.put("wineCategories", wineCategories);
 
         return result;
     }
@@ -126,15 +148,15 @@ public class WineController implements Serializable {
     public static void main(String[] argv) throws IOException {
         //List<Wine> resultList = getWines();
         Wine wine = new Wine();
-        List<Wine> sW = wine.searchForSubstring("Loimer");
+        List<Wine> sW = wine.searchForSubstring("Riesling", "Kamptal", "", "");
         List<Wine> list = wine.searchForString("Loimer");
         List<Wine> wineFrom = wine.searchWinesFromRegion("Wachau");
         List<String> stringList = wine.queryEquivalentClass("Chardonnay");
         List<String> regions = new ArrayList<>();
 
-        Map<String, List<String>> test = semanticSearch("sdgdf");
+        //Map<String, List<String>> test = semanticSearch("sdgdf");
 
-        System.out.println(resultList.get(0).getLabel());
+        //System.out.println(resultList.get(0).getLabel());
         System.out.println(sW.get(0).getLabel());
     }
 }
